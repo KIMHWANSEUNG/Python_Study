6b4504e0a5ccef209f693c3c18480b83f2153cbf
KIMHWANSEUNG <49117761+KIMHWANSEUNG@users.noreply.github.com>
2020-08-05 23:27:35 +0900

    반복문 유트브 강의 2:09:22까지

---
+++ venv/For.py
@@ -0,0 +1,8 @@
+# for
+for waiting_no in range(1,6): #1,2,3,4,5
+    print("대기번호:{0}".format(waiting_no))
+
+#커피 주문
+starbucks=["아이언맨","토르","아이엠 그루트"]
+for customer in starbucks:
+    print("{0}, 커피가 준비되었습니다".format(customer))
