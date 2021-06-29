dc9124ca4aaf6050caa7c47af8292364b9058427
MBKITDE <development.mbkit@daimler.com>
2021-06-29 18:00:32 +0900

    카카오 인턴쉽 코테 풀이 (진행중)

--- 카카오인턴쉽_키패드누르기.py
+++ 카카오인턴쉽_키패드누르기.py
@@ -1,18 +1,106 @@
+
+
+
 def solution(numbers, hand):
-    answer = []
-    for i in numbers:
-        if i in [1,4,7]:
-            answer.append('L')
-        elif i in [3,6,9]:
-            answer.append('R')
-        elif i in [2,5,8,0]:
-            
-           
-    
-    
-        
+    answer=''
+    lhand='*'
+    rhand='#'
+    numbers_dict={1:(0,0),2:(1,0),3:(2,0),
+                  4:(0,1),5:(1,1),6:(2,1),
+                  7:(0,2),8:(1,2),9:(2,2),
+                  '*':(0,3),0:(1,3),'#':(2,3)}
+    for number in numbers :
+        left=[1,4,7]
+        right=[3,6,9]
+        if number in left:
+            answer+='L'
+            lhand=number
+        elif number in right:
+            answer+='R'
+            rhand=number
+        else:
+            lhand_distance=abs(numbers_dict[number][0]-numbers_dict[lhand][0])+abs(numbers_dict[number][1]-numbers_dict[lhand][1])
+            rhand_distance=abs(numbers_dict[number][0]-numbers_dict[rhand][0])+abs(numbers_dict[number][1]-numbers_dict[rhand][1])
+
+            if lhand_distance>rhand_distance:
+                answer+='L'
+                lhand=number
+            elif lhand_distance<rhand_distance:
+                answer+='R'
+                rhand=number
+            else :
+                if hand == 'right':
+                    answer+='R'
+                    rhand=number
+                elif hand == 'left':
+                    answer+='L'
+                    lhand=number           
+
 
     return answer
 
-print(solution([1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5],"right"))
+print(solution([1, 2, 3, 4, 5, 6, 7, 8, 9, 0], "right"))
     #결과는 "LRLLLRLLRRL"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def solution2(numbers, hand):
+    answer = ''
+    key_dict = {1:(0,0),2:(0,1),3:(0,2),
+                4:(1,0),5:(1,1),6:(1,2),
+                7:(2,0),8:(2,1),9:(2,2),
+                '*':(3,0),0:(3,1),'#':(3,2)}
+
+    left = [1,4,7]
+    right = [3,6,9]
+    lhand = '*'
+    rhand = '#'
+    for i in numbers:
+        if i in left:
+            answer += 'L'
+            lhand = i
+        elif i in right:
+            answer += 'R'
+            rhand = i
+        else:
+            curPos = key_dict[i]
+            lPos = key_dict[lhand]
+            rPos = key_dict[rhand]
+            print('rpos',rPos)
+            ldist = abs(curPos[0]-lPos[0]) + abs(curPos[1]-lPos[1])
+            rdist = abs(curPos[0]-rPos[0]) + abs(curPos[1]-rPos[1])
+            
+            if ldist < rdist:
+                answer += 'L'
+                lhand = i
+            elif ldist > rdist:
+                answer += 'R'
+                rhand = i
+            else:
+                if hand == 'left':
+                    answer += 'L'
+                    lhand = i
+                else:
+                    answer += 'R'
+                    rhand = i
+
+    return answer
+
+
+#print(solution2([1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5],"right"))
